b6893ff1167b45db6966a11e7070f2e950bd1b5b
Epitexam <202238868+epitexam@users.noreply.github.com>
2026-06-03 02:25:18 +0200

    feat: add game settings module

---
+++ settings.py
@@ -0,0 +1,16 @@
+"""
+Global game settings.
+
+This module contains all configurable constants used throughout the game.
+"""
+
+# Display
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+FPS = 60
+CAPTION = "Knightrock"
+
+# World
+TILE_SIZE = 64
+
+# Animation
+ANIMATION_SPEED = 6
