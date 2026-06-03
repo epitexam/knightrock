75b702df5a8e89f8b2b7baea9e810175f893a14b
Epitexam <202238868+epitexam@users.noreply.github.com>
2026-06-03 05:18:59 +0200

    feat: add sprite class

---
+++ sprites.py
@@ -0,0 +1,11 @@
+from settings import * 
+import pygame
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, pos,surf, groups, color) -> None:
+        super().__init__(groups)
+
+        # Temporary
+        self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
+        self.image.fill(color)
+        self.rect = self.image.get_frect(topleft = pos)
