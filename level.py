bf1ea1997bed3f6971faf36781083070fccefeb0
Epitexam <202238868+epitexam@users.noreply.github.com>
2026-06-04 03:48:43 +0200

    feat: adding collision system

--- level.py
+++ level.py
@@ -10,15 +10,16 @@ class Level:
     def __init__(self, display_surface, tmx_map):
         self.display_surface = display_surface
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
         self.setup(tmx_map)
 
     def setup(self, tmx_map):
         for x, y, surf in tmx_map.get_layer_by_name("Terrain").tiles():
-            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites, Colors.blue)
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, (self.all_sprites, self.collision_sprites), Colors.blue)
 
         for obj in tmx_map.get_layer_by_name("Objects"):
             if obj.name == "player":
-                Player((obj.x, obj.y), self.all_sprites)
+                Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
 
     def run(self, delta_time):
         self.all_sprites.update(delta_time)
--- player.py
+++ player.py
@@ -7,13 +7,16 @@ from sprites import Sprite
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups) -> None:
+    def __init__(self, pos, groups, collision_sprites) -> None:
         super().__init__(groups)
         self.image = pygame.Surface((48, 56))
         self.image.fill(Colors.green)
         self.rect: pygame.FRect = self.image.get_frect(topleft=pos)
         self.direction = Vector2(1, 0)
         self.speed = SPEED
+        self.collision_sprites = collision_sprites
+        self.old_rect = self.rect.copy()
+        self.gravity = GRAVITY
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -27,11 +30,36 @@ class Player(pygame.sprite.Sprite):
             if self.rect.x > 0:
                 input_vector.x += -1
 
-        self.direction = input_vector.normalize() if input_vector else input_vector
+        self.direction.x = input_vector.normalize().x if input_vector else input_vector.x
 
     def move(self, delta_time):
-        self.rect.topleft += self.direction * self.speed * delta_time
+
+        self.rect.x += self.direction.x * self.speed * delta_time
+        self.collision('horizontal')
+
+        self.direction.y += self.gravity / 2 * delta_time
+        self.rect.y += self.direction.y * delta_time
+        self.direction.y += self.gravity / 2 * delta_time
+        self.collision('vertical')
+
+    def collision(self, axis):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.rect):
+                if axis == "horizontal":
+                    if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right: 
+                        self.rect.left = sprite.rect.right
+                    elif self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:
+                        self.rect.right = sprite.rect.left
+                else:  # vertical
+                    if self.rect.top <= sprite.rect.bottom and self.old_rect.top >= sprite.old_rect.bottom:
+                        self.rect.top = sprite.rect.bottom
+                    elif self.rect.bottom >= sprite.rect.top and self.old_rect.bottom <= sprite.old_rect.top:
+                        self.rect.bottom = sprite.rect.top
+                    self.direction.y = 0
+
 
     def update(self, delta_time):
+        self.old_rect = self.rect.copy()
         self.input()
         self.move(delta_time)
+
